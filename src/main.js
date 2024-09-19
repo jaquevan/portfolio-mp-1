63e6b6bad38be9eea28a3223fdc3dd18e86c20df
jaquevan <jaquevan@bu.edu>
2024-09-18 21:56:58 -0400

    added necessarry html files and finished the JScalculator project without styling

--- src/main.js
+++ src/main.js
@@ -1,23 +1,59 @@
 
+//most of these functions follow the same logic and just use integer operations to
+//make a simple JS calculator web app
 
-function myAdd(){
+let first = document.getElementById("first").value;
+first = Number(first);
 
-}
+let second = document.getElementById("second").value;
+second = Number(second);
 
-function myPow() {
+  function displayResult(result) {
+    let output = document.getElementById("output");
 
-  let first = document.getElementById("first").value;
-  first = Number(first);
-
-  let second = document.getElementById("second").value;
-  second = Number(second);
-
-  let result = 1;
-
-  for (let i = 0; i < second; i++){
-    result = first * result;
+    if (result < 0) {
+      output.innerHTML = `<h3 style="color:red;">${result}</h3>`;
+    } else {
+      output.innerHTML = `<h3>${result}</h3>`;
+    }
   }
 
-  document.getElementById("output").innerHTML=`<h3>${result}</h3>`;
+  function myAdd(){
 
-}
+    let result = first + second;
+    displayResult(result);
+  }
+
+  function mySub(){
+
+    let result = first - second;
+    displayResult(result);  }
+
+  function myMultiply(){
+
+    let result = first * second;
+    displayResult(result);  }
+
+  function myDivide(){
+
+    let result = first / second;
+    displayResult(result);  }
+
+  function myPow() {
+
+    let result = 1;
+
+    for (let i = 0; i < second; i++){
+      result = first * result;
+    }
+    displayResult(result);
+  }
+
+  //make a clear function by just setting an empty string to the output on click
+  function myClear() {
+    document.getElementById("first").value = "";
+    document.getElementById("second").value = "";
+    document.getElementById("output").innerHTML=`<h3>${" "}</h3>`;
+
+
+  }
